913c8ddb2df91962b3c18e86f1b9be70be1800c1
ShocikI <szotmicha16@gmail.com>
2025-01-22 16:33:14 +0100

    Create app to menage backend

--- django_postgis/settings.py
+++ django_postgis/settings.py
@@ -30,7 +30,6 @@ DEBUG = True
 ALLOWED_HOSTS = str(getenv('ALLOWED_HOSTS')).split(",")
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -38,10 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.gis',
+    "django.contrib.gis",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -71,6 +72,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "django_postgis.wsgi.application"
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000', 
+    'http://127.0.0.1:3000',
+]
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -122,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/images/"
+MEDIA_ROOT = "images"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ njoy_backend/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+# Create yout views here.
