66011167f22e810efdb3abe7b5ec82aaa54e9b7d
ShocikI <szotmicha16@gmail.com>
2025-01-29 15:48:06 +0100

    Make links not obligatory

--- njoy_backend/models.py
+++ njoy_backend/models.py
@@ -15,7 +15,7 @@ class LinkType(models.Model):
     
 
 class UserLink(models.Model):
-    type = models.ForeignKey(LinkType, verbose_name=("Link type"), on_delete=models.CASCADE, blank=True)
+    type = models.ForeignKey(LinkType, verbose_name=("Link type"), on_delete=models.CASCADE, blank=False)
     link_url = models.TextField(default="", blank=False)
     
     def __str__(self):
@@ -23,7 +23,7 @@ class UserLink(models.Model):
     
 
 class EventLink(models.Model):
-    type = models.ForeignKey(LinkType, verbose_name=("Link type"), on_delete=models.CASCADE, blank=True)
+    type = models.ForeignKey(LinkType, verbose_name=("Link type"), on_delete=models.CASCADE, blank=False)
     link_url = models.TextField(default="", blank=False)
     
     def __str__(self):
@@ -44,7 +44,7 @@ class Categories(models.Model):
 class User(AuthUser):
     description = models.TextField(blank=True)
     avatar = models.ImageField(upload_to="images/avatars", blank=True)
-    links = geoModels.ForeignKey(UserLink, verbose_name=("Links"), on_delete=geoModels.CASCADE, blank=False)
+    links = geoModels.ForeignKey(UserLink, verbose_name=("Links"), on_delete=geoModels.CASCADE, blank=True, null=True)
     plus = models.IntegerField(default=0)
     minus = models.IntegerField(default=0)
 
@@ -57,11 +57,11 @@ class Event(geoModels.Model):
     title = geoModels.CharField(max_length=128, blank=False)
     owner = geoModels.ForeignKey(User, verbose_name=("Owner"), on_delete=geoModels.CASCADE, blank=False)
     date = geoModels.DateTimeField(auto_now=False, auto_now_add=False, blank=False)
-    category = geoModels.ForeignKey(Categories, verbose_name=("Category"), on_delete=geoModels.CASCADE)
-    address = geoModels.CharField(max_length=255)
+    category = geoModels.ForeignKey(Categories, verbose_name=("Category"), on_delete=geoModels.CASCADE, blank=False)
+    address = geoModels.CharField(max_length=255, blank=False)
     location = geoModels.PointField(geography=True, blank=False, null=False)
      
-    links = geoModels.ForeignKey(EventLink, verbose_name=("Links"), on_delete=geoModels.CASCADE, blank=False)
+    links = geoModels.ForeignKey(EventLink, verbose_name=("Links"), on_delete=geoModels.CASCADE, blank=True, null=True)
     image = geoModels.ImageField(upload_to="images/posters", height_field=None, width_field=None, max_length=None, blank=True)
     description = geoModels.TextField(max_length=2048, blank=True)
     price = geoModels.FloatField(default=0.0, blank=True)
