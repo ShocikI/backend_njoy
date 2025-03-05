68bc718451b380bc3ee6fbe2c986933a03fa747b
ShocikI <szotmicha16@gmail.com>
2025-03-05 12:54:47 +0100

    Improve querypParams

--- njoy_backend/views.py
+++ njoy_backend/views.py
@@ -1,9 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.measure import D
 from datetime import datetime
 
+
 from njoy_backend.serializers import (
-    UserSerializer, User, 
+    RegistrationSerializer, User, 
     EventSerializer, Event, 
     CategorySerializer, Categories,
     LinkTypeSerializer, LinkType, 
@@ -13,33 +17,56 @@ from njoy_backend.serializers import (
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
+    serializer_class = RegistrationSerializer
     queryset = User.objects.all()
+
 
 class UpcommingEventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
+    queryset = Event.objects.all()
 
     def list(self, request, *args, **kwargs):
         today = datetime.now()
-        categoryIds = self.request.query_params.getlist('category')
-        if categoryIds:
-            queryset = Event.objects.filter(date__gt=today).filter(category__in=categoryIds)
-        else:
+        point = self.request.query_params.getlist('point')
+        radius = self.request.query_params.getlist('radius')
+        categoryIds = self.request.query_params.getlist('categoryIds')
+
+        if point and radius:
+            # Prepare point data
+            lon, lat = str(*point).split("-")
+            lon = float(lon)
+            lat = float(lat)
+            current_point = Point(lon, lat, srid=4326)
+            current_radius = float(radius[0])
+
+            # Get the not expired events
             queryset = Event.objects.filter(date__gt=today)
+            # Get events in radius from user position
+            queryset = queryset.filter(
+                location__distance_lte=(current_point, D(km=current_radius))
+                ).annotate(
+                    distance=Distance('location', current_point)
+            )
+            # Get event according to category
+            if categoryIds != ['']:
+                queryset = queryset.filter(category__in=categoryIds)
 
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-class ArchiveEventViewSet(viewsets.ModelViewSet):
+class PreviousEventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
+    queryset = Event.objects.all()
 
     def list(self, request, *args, **kwargs):
         today = datetime.now()
+        queryset = Event.objects.filter(date__lt=today)
+
         categoryIds = self.request.query_params.getlist('category')
-        if categoryIds:
-            queryset = Event.objects.filter(date__lt=today).filter(category__in=categoryIds)
-        else:
-            queryset = Event.objects.filter(date__lt=today)
+        if categoryIds != ['']:
+            queryset = queryset.filter(category__in=categoryIds)
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
