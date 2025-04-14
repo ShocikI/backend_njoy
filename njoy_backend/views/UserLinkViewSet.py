049b8e732306fc5c37148213f6189f75837efc43
ShocikI <szotmicha16@gmail.com>
2025-04-14 20:54:41 +0200

    Add authentication, permissions and create method to UserLinkViewSet

--- njoy_backend/views/UserLinkViewSet.py
+++ njoy_backend/views/UserLinkViewSet.py
@@ -1,7 +1,30 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
-from njoy_backend.serializers import ( UserLinkSerializer, UserLink )
+from django_postgis.authentication import EncryptedTokenAuthentication
+from njoy_backend.serializers import ( UserLinkSerializer, UserLink, User )
 
 class UserLinkViewSet(viewsets.ModelViewSet):
     serializer_class = UserLinkSerializer
+    authentication_classes = [EncryptedTokenAuthentication]
     queryset = UserLink.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+        
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['owner_id'] = request.user.id
+
+        serializer = self.get_serializer(data=data)
+        if not serializer.is_valid():
+            print(f"[UserLink] Błąd walidacji: {serializer.errors}")
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def perform_create(self, serializer):
+        serializer.save()
