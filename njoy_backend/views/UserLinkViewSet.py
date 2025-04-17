277e96f666562500cc9d4648146ee73dbd5f1e25
ShocikI <szotmicha16@gmail.com>
2025-04-17 12:31:25 +0200

    Merge UserLinksByUsernameView with UserLinkViewSet, simplify UserLinkViewSet

--- njoy_backend/views/UserLinkViewSet.py
+++ njoy_backend/views/UserLinkViewSet.py
@@ -1,31 +1,29 @@
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from django_postgis.authentication import EncryptedTokenAuthentication
+from njoy_backend.permissions import isOwnerOrReadOnly
 from njoy_backend.serializers import ( UserLinkSerializer )
-from njoy_backend.models import ( UserLink )
+from njoy_backend.models import ( UserLink, User )
 
 class UserLinkViewSet(viewsets.ModelViewSet):
     serializer_class = UserLinkSerializer
     authentication_classes = [EncryptedTokenAuthentication]
     queryset = UserLink.objects.all()
-
-    def get_permissions(self):
-        if self.action in ['create', 'update', 'partial_update', 'destroy']:
-            return [permissions.IsAuthenticated()]
-        return [permissions.AllowAny()]
+    permission_classes = [isOwnerOrReadOnly]
         
-    def create(self, request, *args, **kwargs):
-        data = request.data.copy()
-        data['owner_id'] = request.user.id
+    def get_queryset(self):
+        username = self.kwargs.get('username')
+        if username:
+            user = get_object_or_404(User, username=username)
+            return self.queryset.filter(owner=user)
+        return None
 
-        serializer = self.get_serializer(data=data)
-        if not serializer.is_valid():
-            print(f"[UserLink] Błąd walidacji: {serializer.errors}")
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        
-        self.perform_create(serializer)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
     def perform_create(self, serializer):
-        serializer.save()
+        username = self.kwargs.get('username')
+        if username:
+            user = get_object_or_404(User, username=username)
+            serializer.save(owner=user)
+        else:
+            serializer.save(owner=self.request.user)
