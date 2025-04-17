75ba14cf2c30d31e7d8e4f629b7e09983af7c3f9
ShocikI <szotmicha16@gmail.com>
2025-04-17 10:48:47 +0200

    Add EventLinkViewSet create method

--- njoy_backend/views/EventLinkViewSet.py
+++ njoy_backend/views/EventLinkViewSet.py
@@ -1,8 +1,31 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
-from njoy_backend.serializers import ( EventLinkSerializer, EventLink )
-
+from django_postgis.authentication import EncryptedTokenAuthentication
+from njoy_backend.serializers import ( EventLinkSerializer )
+from njoy_backend.models import ( EventLink )
 
 class EventLinkViewSet(viewsets.ModelViewSet):
     serializer_class = EventLinkSerializer
+    authentication_classes = [EncryptedTokenAuthentication]
     queryset = EventLink.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['owner_id'] = request.user.id
+
+        serializer = self.get_serializer(data=data)
+        if not serializer.is_valid():
+            print(f"[EventLink] Błąd walidacji: {serializer.errors}")
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def perform_create(self, serializer):
+        serializer.save()
