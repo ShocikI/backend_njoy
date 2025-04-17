01c01395b1af32df13f6e126ac1df0a8c920ce29
ShocikI <szotmicha16@gmail.com>
2025-04-17 12:30:07 +0200

    Add isOwnerOrReadOnly permission class

---
+++ njoy_backend/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+class isOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if request.method == 'POST':
+            username = view.kwargs.get('username')
+            return request.user.is_authenticated and username == request.user.username
+
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.owner == request.user
