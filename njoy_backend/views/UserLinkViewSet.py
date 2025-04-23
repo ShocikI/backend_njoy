5a16aa4c353a3da78f76fd2bf6bd88d6011433e2
ShocikI <szotmicha16@gmail.com>
2025-04-23 11:48:50 +0200

    Simplify imports

--- njoy_backend/views/UserLinkViewSet.py
+++ njoy_backend/views/UserLinkViewSet.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, permissions, status
-from rest_framework.response import Response
+from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
 from django_postgis.authentication import EncryptedTokenAuthentication
@@ -24,4 +23,3 @@ class UserLinkViewSet(viewsets.ModelViewSet):
         username = self.kwargs.get("username_username")
         user = get_object_or_404(User, username=username)
         serializer.save(owner=user)
-
