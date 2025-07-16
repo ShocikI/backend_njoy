8779ac2e4bb776dc005da5efbaf0b8948f2ce66a
ShocikI <szotmicha16@gmail.com>
2025-07-16 13:23:56 +0200

    Add username as sufix to avatar

--- njoy_backend/views/UserViewSet.py
+++ njoy_backend/views/UserViewSet.py
@@ -28,7 +28,7 @@ class UserViewSet(viewsets.ModelViewSet):
                 return [ permissions.IsAuthenticated() ]
 
 
-    def retrieve(self, request, username=None):
+    def retrieve(self, request, username:str=None):
         try:
             queryset = User.objects.get(username=username)
         except User.DoesNotExist:
@@ -37,15 +37,14 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(queryset)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def update(self, request, username=None, *args, **kwargs):
+    def update(self, request, username:str=None, *args, **kwargs):
         partial = kwargs.pop('partial', True)
 
         try:
-            queryset = User.objects.get(username=username)
+            user = User.objects.get(username=username)
         except User.DoesNotExist:
             return Response(None, status=status.HTTP_404_NOT_FOUND)
 
-        allowed_fields = {'avatar', 'description'}
         requested_fields = set(request.data.keys())
 
         if len(requested_fields) != 1:
@@ -55,14 +54,20 @@ class UserViewSet(viewsets.ModelViewSet):
             )
 
         if 'avatar' in request.data:
-            new_avatar = request.data.get('avatar')
-            old_avatar = queryset.avatar
+            old_avatar = user.avatar
+            new_avatar = request.data['avatar']
+
+            name, format = new_avatar.name.split(".")
+            new_avatar_name = f"{name}_{username.lower()}.{format}"
+
             if new_avatar and old_avatar and hasattr(new_avatar, 'name'):
                 if old_avatar.name != new_avatar.name:
                     if hasattr(old_avatar, 'path') and os.path.isfile(old_avatar.path):
                         os.remove(old_avatar.path)
 
-        serializer = self.get_serializer(queryset, data=request.data, partial=partial)
+            request.data['avatar'].name = new_avatar_name
+
+        serializer = self.get_serializer(user, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
