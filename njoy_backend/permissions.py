0bd090241619c2bf84830caea21f175bffd3b2d3
ShocikI <szotmicha16@gmail.com>
2025-07-17 12:23:57 +0200

    Create permission class to manage eventLinks

--- njoy_backend/permissions.py
+++ njoy_backend/permissions.py
@@ -1,4 +1,7 @@
 from rest_framework import permissions
+from django.shortcuts import get_object_or_404
+
+from njoy_backend.models import Event
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
@@ -6,10 +9,31 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         
-        username = view.kwargs.get('username_username')
-        return request.user.is_authenticated and username == request.user.username
+        username = view.kwargs.get('username')
+        return (
+            request.user.is_authenticated 
+            and username == request.user.username
+        )
+    
     
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+class IsEventOwnerOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        event_pk = view.kwargs.get('event_pk')
+        if not event_pk:
+            return False
+
+        event = get_object_or_404(Event, pk=event_pk)
+        return request.user.is_authenticated and event.owner == request.user
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.event.owner == request.user
--- njoy_backend/views/EventLinkViewSet.py
+++ njoy_backend/views/EventLinkViewSet.py
@@ -2,24 +2,24 @@ from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
 from django_postgis.authentication import EncryptedTokenAuthentication
-from njoy_backend.permissions import IsOwnerOrReadOnly
+from njoy_backend.permissions import IsEventOwnerOrReadOnly
 from njoy_backend.serializers import ( EventLinkSerializer )
 from njoy_backend.models import ( User, Event, EventLink )
 
 class EventLinkViewSet(viewsets.ModelViewSet):
     serializer_class = EventLinkSerializer
     authentication_classes = [ EncryptedTokenAuthentication ]
-    permission_classes = [ IsOwnerOrReadOnly ]
+    permission_classes = [ IsEventOwnerOrReadOnly ]
     queryset = EventLink.objects.all()
 
     def get_queryset(self):
         pk = self.kwargs.get('event_pk')
         if pk:
-            user = get_object_or_404(Event, pk=pk)
-            return EventLink.objects.filter(owner=user)
+            event = get_object_or_404(Event, pk=pk)
+            return EventLink.objects.filter(owner=event)
         return EventLink.objects.none()
 
     def perform_create(self, serializer):
-        username = self.kwargs.get("username_username")
-        user = get_object_or_404(User, username=username)
-        serializer.save(owner=user)
+        event_pk = self.kwargs.get("event_pk")
+        event = get_object_or_404(Event, pk=event_pk)
+        serializer.save(owner=event)
